6480d6694ef6d3b7c5daa0fa65426f9d80ad6bed
TMarkos <citizen@logoma.ch>
2014-08-20 20:41:58 -0400

    Added Techboxes, some structure changes.
    Created Techboxes (the "part" portion of #5, #6, #7 and #8)
    Resolved #8 - SCU logic was pretty easy.
    Created general framework to allow beacons to check and report which
    tech modules are attached and active.
    Split out ESLDHailer and ESLDBeacon into their own files.
    Modified code to address #16, but I still need to test that it's
    working.

---
+++ Source/ESLDTechbox.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ESLDCore
+{
+    public class ESLDTechbox : PartModule
+    {
+        [KSPField(guiName = "Status", isPersistant = true, guiActive = true)]
+        public string techBoxStatus;
+
+        [KSPField(isPersistant = true, guiActive = false)]
+        public bool activated = false;
+
+        [KSPField(isPersistant = true, guiActive = false)]
+        public string techBoxModel;
+
+        public override void OnUpdate()
+        {
+            ModuleAnimateGeneric MAG = part.FindModuleImplementing<ModuleAnimateGeneric>();
+            MAG.Events["Toggle"].guiActive = false;
+            if (activated && MAG.Progress == 0)
+            {
+                MAG.Toggle();
+            }
+            else if (!activated && MAG.Progress == 1)
+            {
+                MAG.Toggle();
+            }
+        }
+
+        [KSPEvent(name = "TechBoxOn", active = true, guiActive = true, guiName = "Activate")]
+        public void TechBoxOn()
+        {
+            part.force_activate();
+            activated = true;
+            techBoxStatus = techBoxModel + " Active.";
+            Events["TechBoxOn"].active = false;
+            Events["TechBoxOff"].active = true;
+            foreach (ESLDBeacon beacon in vessel.FindPartModulesImplementing<ESLDBeacon>())
+            {
+                beacon.checkOwnTechBoxes();
+            }
+        }
+
+        [KSPEvent(name = "TechBoxOff", active = false, guiActive = true, guiName = "Deactivate")]
+        public void TechBoxOff()
+        {
+            part.deactivate();
+            activated = false;
+            techBoxStatus = techBoxModel + " Inactive.";
+            Events["TechBoxOn"].active = true;
+            Events["TechBoxOff"].active = false;
+        }
+    }
+}
