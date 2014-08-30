0928f4d3fe1bbf8aec242989b1ad9006fd80e3cd
TMarkos <citizen@logoma.ch>
2014-08-30 14:01:54 -0400

    Electric Use for Beacons, some cleanup
    Reorganized ESLDBeacon, added some comments.
    Resolved #25, beacons now use a large initial amount of EC followed by a
    constant charge determined by the ratio of local gravity to the gravity
    limit as well as the total amount of KBD on board.
    The GMU now incurs a 50% penalty to beacon EC use, both activation and
    operation charges.

--- Source/ESLDBeacon.cs
+++ Source/ESLDBeacon.cs
@@ -9,7 +9,100 @@ namespace ESLDCore
 {
     public class ESLDBeacon : PartModule
     {
-        public object targetbody;
+        // Display beacon status in right click menu.
+        [KSPField(guiName = "Beacon Status", isPersistant = true, guiActive = true)]
+        public string beaconStatus;
+
+        // Per-beacon G limit.
+        [KSPField(isPersistant=true, guiActive=false)]
+        public double gLimit = 2;
+
+        // Activated state.
+        [KSPField(isPersistant = true, guiActive = false)]
+        public bool activated = false;
+
+        // Self-reported fuel quantity.
+        [KSPField(isPersistant = true, guiActive = false)]
+        public double fuelOnBoard = 0;
+
+        // Beacon model (from part config).
+        [KSPField(isPersistant = true, guiActive = false)]
+        public string beaconModel;
+
+        // Self-reported binary techbox capability.  
+        [KSPField(isPersistant = true, guiActive = false)]
+        public int techBoxInventory = 0;
+
+        // Display beacon operational floor in right click menu.
+        [KSPField(guiName = "Lowest Altitude", isPersistant = false, guiActive = true, guiUnits = "km")]
+        public double opFloor;
+
+        // Charge to initialize beacon.
+        [KSPField(guiName = "Charge to Activate", isPersistant = false, guiActive = false, guiUnits = " EC")]
+        public double neededEC;
+
+        // Charge to run beacon.
+        [KSPField(guiName = "Electric Use", isPersistant = false, guiActive = false, guiUnits = " EC/s")]
+        public double constantEC;
+
+        public bool hasAMU = false;
+        public bool hasHCU = false;
+        public bool hasGMU = false;
+        public bool hasSCU = false;
+
+        public override void OnUpdate()
+        {
+            opFloor = findAcceptableAltitude(vessel.mainBody); // Keep updating tooltip display.
+            if (FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude <= gLimit) Fields["neededEC"].guiActive = !activated;
+            Fields["constantEC"].guiActive = activated;
+            ModuleAnimateGeneric MAG = part.FindModuleImplementing<ModuleAnimateGeneric>();
+            MAG.Events["Toggle"].guiActive = false;
+            if (activated && MAG.Progress == 0)
+            {
+                MAG.Toggle();
+            } else if (!activated && MAG.Progress == 1)
+            {
+                MAG.Toggle();
+            }
+            double vfuel = 0;
+            foreach (Part vpart in vessel.Parts)
+            {
+                foreach (PartResource vpr in vpart.Resources)
+                {
+                    if (vpr.resourceName == "Karborundum") vfuel += vpr.amount;
+                }
+            }
+            fuelOnBoard = vfuel;
+        }
+
+        public override void OnFixedUpdate()
+        {
+            checkOwnTechBoxes();
+            if (FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude > gLimit)
+            {
+                ScreenMessages.PostScreenMessage("Warning: Too deep in gravity well.  Beacon has been shut down for safety.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                BeaconShutdown();
+            }
+            if (vessel.altitude < (vessel.mainBody.Radius * .25f))
+            {
+                string thevar = "";
+                if (vessel.mainBody.name == "Mun" || vessel.mainBody.name == "Sun") thevar = "the ";
+                ScreenMessages.PostScreenMessage("Warning: Too close to " + thevar + vessel.mainBody.name + ".  Beacon has been shut down for safety.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                BeaconShutdown();
+            }
+
+            if (!requireResource(vessel, "ElectricCharge", Time.deltaTime * constantEC, true))
+            {
+                ScreenMessages.PostScreenMessage("Warning: Electric Charge depleted.  Beacon has been shut down.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                BeaconShutdown();
+            }
+
+            if (!requireResource(vessel, "Karborundum", 0.1, false))
+            {
+                ScreenMessages.PostScreenMessage("Warning: Karborundum depleted.  Beacon has been shut down.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                BeaconShutdown();
+            }
+        }
 
         // Given a target body, get minimum ASL where beacon can function in km.
         public double findAcceptableAltitude(CelestialBody targetbody)
@@ -32,52 +125,19 @@ namespace ESLDCore
                     gLimit = 0.1;
                     break;
             }
-            if (hasGMU) gLimit *= 1.25;
+            double neededMult = 10;
+            double constantDiv = 50;
+            if (hasGMU)
+            {
+                gLimit *= 1.25;
+                neededMult = 15;
+                constantDiv = 100 / 3;
+            }
             double limbo = Math.Round((Math.Sqrt((6.673E-11 * targetbody.Mass) / gLimit) - targetbody.Radius));
             if (limbo < targetbody.Radius * 0.25) limbo = targetbody.Radius * 0.25;
+            neededEC = Math.Round(fuelOnBoard * neededMult * (FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude / gLimit));
+            constantEC = Math.Round(fuelOnBoard / constantDiv * (FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude / gLimit) * 100) / 100;
             return limbo / 1000;
-        }
-
-        // Display beacon status in right click menu.
-        [KSPField(guiName = "Beacon Status", isPersistant = true, guiActive = true)]
-        public string beaconStatus;
-
-        [KSPField(isPersistant=true, guiActive=false)]
-        public double gLimit = 2;
-
-        [KSPField(isPersistant = true, guiActive = false)]
-        public bool activated = false;
-
-        [KSPField(isPersistant = true, guiActive = false)]
-        public double fuelOnBoard = 0;
-
-        [KSPField(isPersistant = true, guiActive = false)]
-        public string beaconModel;
-
-        [KSPField(isPersistant = true, guiActive = false)]
-        public int techBoxInventory = 0;
-
-        // Display beacon operational floor in right click menu.
-        [KSPField(guiName = "Lowest Altitude", isPersistant = false, guiActive = true, guiUnits="km")]
-        public double opFloor;
-
-        public bool hasAMU = false;
-        public bool hasHCU = false;
-        public bool hasGMU = false;
-        public bool hasSCU = false;
-
-        public override void OnUpdate()
-        {
-            opFloor = findAcceptableAltitude(vessel.mainBody); // Keep updating tooltip display.
-            ModuleAnimateGeneric MAG = part.FindModuleImplementing<ModuleAnimateGeneric>();
-            MAG.Events["Toggle"].guiActive = false;
-            if (activated && MAG.Progress == 0)
-            {
-                MAG.Toggle();
-            } else if (!activated && MAG.Progress == 1)
-            {
-                MAG.Toggle();
-            }
         }
 
         public void checkOwnTechBoxes()
@@ -119,73 +179,94 @@ namespace ESLDCore
             }
         }
 
+        // Simple bool for resource checking and usage.  Returns true and optionally uses resource if resAmount of res is available.
+        public bool requireResource(Vessel craft, string res, double resAmount, bool consumeResource)
+        {
+            if (!craft.loaded) return false; // Unloaded resource checking is unreliable.
+            Dictionary<PartResource, double> toDraw = new Dictionary<PartResource,double>();
+            double resRemaining = resAmount;
+            foreach (Part cPart in craft.Parts)
+            {
+                foreach (PartResource cRes in cPart.Resources)
+                {
+                    if (cRes.resourceName != res) continue;
+                    if (cRes.amount == 0) continue;
+                    if (cRes.amount >= resRemaining)
+                    {
+                        toDraw.Add(cRes, resRemaining);
+                        resRemaining = 0;
+                    } else
+                    {
+                        toDraw.Add(cRes, cRes.amount);
+                        resRemaining -= cRes.amount;
+                    }
+                }
+                if (resRemaining <= 0) break;
+            }
+            if (resRemaining > 0) return false;
+            if (consumeResource)
+            {
+                foreach (KeyValuePair<PartResource, double> drawSource in toDraw)
+                {
+                    drawSource.Key.amount -= drawSource.Value;
+                }
+            }
+            return true;
+        }
+
         // Startup sequence for beacon.
         [KSPEvent(name="BeaconInitialize", active = true, guiActive = true, guiName = "Initialize Beacon")]
         public void BeaconInitialize()
         {
             checkOwnTechBoxes();
+            if (!requireResource(vessel, "Karborundum", 0.1, false))
+            {
+                ScreenMessages.PostScreenMessage("Cannot activate!  Insufficient Karborundum to initiate reaction.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                return;
+            }
             if (FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude > gLimit) // Check our G forces.
             {
                 print("Too deep in gravity well to activate!");
                 string thevar = "";
                 if (vessel.mainBody.name == "Mun" || vessel.mainBody.name == "Sun") thevar = "the ";
                 ScreenMessages.PostScreenMessage("Cannot activate!  Gravity from " + thevar + vessel.mainBody.name + " is too strong.",5.0f,ScreenMessageStyle.UPPER_CENTER);
-                BeaconShutdown();
+                return;
             }
-            else if (vessel.altitude < (vessel.mainBody.Radius * .25f))
+            if (vessel.altitude < (vessel.mainBody.Radius * .25f)) // Check for radius limit.
             {
                 string thevar = "";
                 if (vessel.mainBody.name == "Mun" || vessel.mainBody.name == "Sun") thevar = "the ";
                 ScreenMessages.PostScreenMessage("Cannot activate!  Beacon is too close to " + thevar + vessel.mainBody.name + ".", 5.0f, ScreenMessageStyle.UPPER_CENTER);
-                BeaconShutdown();
-            } else
-            {
-                ModuleAnimateGeneric MAG = part.FindModuleImplementing<ModuleAnimateGeneric>();
-                print("Activating beacon!  Toggling MAG from " + MAG.status + "-" + MAG.Progress);
-                MAG.Toggle();
-                activated = true;
-                part.force_activate();
-                Events["BeaconInitialize"].active = false;
-                Events["BeaconShutdown"].active = true;
-                beaconStatus = "Active.";
+                return;
             }
-        }
-
-        public override void OnFixedUpdate()
-        {
-            checkOwnTechBoxes();
-            if (FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude > gLimit)
+            if (!requireResource(vessel, "ElectricCharge", neededEC, true))
             {
-                ScreenMessages.PostScreenMessage("Warning: Too deep in gravity well.  Beacon has been shut down for safety.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
-                BeaconShutdown();
+                ScreenMessages.PostScreenMessage("Cannot activate!  Insufficient electric power to initiate reaction.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                return;
             }
-            if (vessel.altitude < (vessel.mainBody.Radius * .25f))
-            {
-                string thevar = "";
-                if (vessel.mainBody.name == "Mun" || vessel.mainBody.name == "Sun") thevar = "the ";
-                ScreenMessages.PostScreenMessage("Warning: Too close to " + thevar + vessel.mainBody.name + ".  Beacon has been shut down for safety.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
-                BeaconShutdown();
-            }
-            double vfuel = 0;
-            foreach (Part vpart in vessel.Parts)
-            {
-                foreach (PartResource vpr in vpart.Resources)
-                {
-                    if (vpr.resourceName == "Karborundum") vfuel += vpr.amount;
-                }
-            }
-            fuelOnBoard = vfuel;
+            ModuleAnimateGeneric MAG = part.FindModuleImplementing<ModuleAnimateGeneric>();
+            print("Activating beacon!  Toggling MAG from " + MAG.status + "-" + MAG.Progress);
+            print("EC Activation charge at " + neededEC + "(" + FlightGlobals.getGeeForceAtPosition(vessel.GetWorldPos3D()).magnitude + "/" + gLimit + ", " + fuelOnBoard + ")");
+            MAG.Toggle();
+            activated = true;
+            part.force_activate();
+            Fields["neededEC"].guiActive = false;
+            Fields["constantEC"].guiActive = true;
+            Events["BeaconInitialize"].active = false;
+            Events["BeaconShutdown"].active = true;
+            beaconStatus = "Active.";
         }
 
         [KSPEvent(name = "BeaconShutdown", active = false, guiActive = true, guiName = "Shutdown")]
         public void BeaconShutdown()
         {
             ModuleAnimateGeneric MAG = part.FindModuleImplementing<ModuleAnimateGeneric>();
-            print("Activating beacon!  Toggling MAG from " + MAG.status + "-" + MAG.Progress);
+            print("Deactivating beacon!  Toggling MAG from " + MAG.status + "-" + MAG.Progress);
             MAG.Toggle();
             beaconStatus = "Offline.";
             part.deactivate();
             activated = false;
+            Fields["constantEC"].guiActive = false;
             Events["BeaconShutdown"].active = false;
             Events["BeaconInitialize"].active = true;
         }
