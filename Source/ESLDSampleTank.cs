f2c5b676256014082fd8a911ccf8324e9db72872
TMarkos <citizen@logoma.ch>
2014-08-30 23:19:45 -0400

    Add ESLDSampleTank class
    This addresses #39 - need to texture the part before I close it, but
    here's the functionality.  The module wipes out most of the canister's
    value (to an extent modifiable in the cfg) when any resources are
    removed from the tank, preventing abuse of vehicle recovery to get
    around forced scarcity.  @BobPalmer, you may be interested.

---
+++ Source/ESLDSampleTank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESLDCore
+{
+    class ESLDSampleTank : PartModule, IPartCostModifier
+    {
+        [KSPField(isPersistant=true, guiActive=false)]
+        public bool canisterSealed = true;
+        [KSPField(isPersistant = false, guiActive = false)]
+        public float markdown;  // Set to negative number in part.cfg file.
+
+        public override void OnUpdate()
+        {
+            foreach (PartResource pR in part.Resources)
+            {
+                if (pR.amount < pR.maxAmount && canisterSealed)
+                {
+                    canisterSealed = false;
+                }
+            }
+        }
+
+        public float GetModuleCost()
+        {
+            if (!canisterSealed) return markdown;
+            return 0.0f;
+        }
+    }
+}
